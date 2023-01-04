bae36be2951bf5791d67356f44374fe6da156472
Jang-geun-Lee <117253261+Jang-geun-Lee@users.noreply.github.com>
2023-01-04 15:00:42 +0900

    [Bronze V] Title: 시험 성적, Time: 128 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int a = Integer.parseInt(br.readLine());
+        
+        if(a >= 90 && a <= 100){
+            System.out.print("A");
+        }
+        else if(a >= 80 && a <= 89){
+            System.out.print("B");
+        }
+        else if(a >= 70 && a <= 79){
+            System.out.print("C");
+        }
+        else if(a >= 60 && a <= 69){
+            System.out.print("D");
+        }
+        else{
+            System.out.print("F");
+        }
+        br.close();
+    }
+}
