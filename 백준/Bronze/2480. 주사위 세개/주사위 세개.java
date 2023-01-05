c03d632acd9ae1660f54c75515921239cf084fea
Jang-geun-Lee <117253261+Jang-geun-Lee@users.noreply.github.com>
2023-01-05 15:30:30 +0900

    [Bronze IV] Title: 주사위 세개, Time: 128 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        
+        if(a == b && b == c){
+            System.out.print(10000+a*1000);
+        }
+        else if(a == b && b != c){
+            System.out.print(1000+a*100);
+        }
+        else if(a == c && b != c){
+            System.out.print(1000+a*100);
+        }
+        else if(b == c && a != c){
+            System.out.print(1000+b*100);
+        }
+        else if(a != b && b != c && a != c){
+            if(a > b && a > c){
+                System.out.print(100*a);
+            }
+            else if(b > a && b > c){
+                System.out.print(b*100);
+            }
+            else if(c > a && c > b){
+                System.out.print(c*100);
+            }
+        }
+        
+        br.close();
+    }
+}
