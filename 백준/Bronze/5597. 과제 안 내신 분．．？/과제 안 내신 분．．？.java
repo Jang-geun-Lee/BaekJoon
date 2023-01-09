69ad5e6903554377bb109066ecf4398cfe7de875
Jang-geun-Lee <117253261+Jang-geun-Lee@users.noreply.github.com>
2023-01-09 13:40:49 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 120 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String args[]) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        boolean[] studentNumArray = new boolean[31];
+
+        for(int i = 1; i < 29; i++){
+            st = new StringTokenizer((br.readLine()), " ");
+            studentNumArray[Integer.parseInt(st.nextToken())] = true;
+        }
+
+        for(int i = 1; i <= 30; i++){
+            if(!studentNumArray[i]){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
