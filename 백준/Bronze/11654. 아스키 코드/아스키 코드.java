8a274365880b5b3124eb97e00696732f6a85571c
Jang-geun-Lee <117253261+Jang-geun-Lee@users.noreply.github.com>
2023-01-11 12:58:00 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Main{
+
+    public static void main(String[] args) throws IOException{
+
+        int str = System.in.read();
+        System.out.print(str);
+    }
+}
