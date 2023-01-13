8410a9696f71ca77441031e933620923a8f80015
Jang-geun-Lee <117253261+Jang-geun-Lee@users.noreply.github.com>
2023-01-13 18:22:21 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 120 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main{
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(stringTokenizer.nextToken());
+        int b = Integer.parseInt(stringTokenizer.nextToken());
+        int v = Integer.parseInt(stringTokenizer.nextToken());
+        int count = (v - b) / (a - b);
+
+        if((v - b) % (a - b) != 0){
+            count++;
+        }
+        System.out.print(count);
+    }
+}
