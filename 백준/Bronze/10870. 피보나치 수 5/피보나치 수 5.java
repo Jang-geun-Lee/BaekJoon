03eee3e95fff73661362dfb4a15f9562e5debb02
Jang-geun-Lee <117253261+Jang-geun-Lee@users.noreply.github.com>
2023-02-07 13:36:17 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int result = cal(n);
+        System.out.print(result);
+
+    }
+    public static int cal(int n){
+        if(n == 0){
+            return 0;
+        }
+        else if(n == 1)
+        {
+            return 1;
+        }
+        return cal(n - 1) + cal(n - 2);
+    }
+}
